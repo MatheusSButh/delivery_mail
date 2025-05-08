d148c3ceb51c01ff49cc2d47dd1d7516c892704d
Matheus <matheusdsouzabuth@gmail.com>
2025-05-08 17:36:11 -0300

    ViaCep Service

---
+++ src/main/java/com/buthdev/demo/services/ViaCepService.java
@@ -0,0 +1,19 @@
+package com.buthdev.demo.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.buthdev.demo.model.Address;
+
+@Service
+public class ViaCepService {
+	
+	public Address checkAddress(String cep) {
+		String url = "https://viacep.com.br/ws/" + cep + "/json/";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		Address address = restTemplate.getForObject(url, Address.class);
+		
+		return address;
+	}
+}
