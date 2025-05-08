9062affd38e4b2b2448504d80509e364477fb5fa
Matheus <matheusdsouzabuth@gmail.com>
2025-05-08 17:01:22 -0300

    First commit

---
+++ src/main/java/com/buthdev/demo/DeliveryMailApplication.java
@@ -0,0 +1,13 @@
+package com.buthdev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryMailApplication.class, args);
+	}
+
+}
