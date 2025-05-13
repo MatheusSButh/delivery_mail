47b0f0d67db60acd9fef2bd6ea51bd897919f38b
Matheus <matheusdsouzabuth@gmail.com>
2025-05-13 10:32:11 -0300

    Unit test, create item with null arguments

--- src/test/java/com/buthdev/demo/testservices/ItemServiceTest.java
+++ src/test/java/com/buthdev/demo/testservices/ItemServiceTest.java
@@ -2,11 +2,14 @@ package com.buthdev.demo.testservices;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -19,29 +22,63 @@ import com.buthdev.demo.services.ItemService;
 @ExtendWith(MockitoExtension.class)
 public class ItemServiceTest {
 
-    @InjectMocks
-    private ItemService itemService;
-
     @Mock
     private ItemRepository itemRepository;
+    
+    @InjectMocks
+    private ItemService itemService;
+    
+    @Captor
+    private ArgumentCaptor<Item> itemArgumentCaptor;
 
     @Test
-    void createItem() {
+    void shouldCreateItem() {
 
+    	//Arrange
         ItemDTO itemDto = new ItemDTO("Produto teste", 50.00);
 
         Item item = new Item();
         item.setId(1L);
         item.setName(itemDto.name());
         item.setValue(itemDto.value());
+        
+        when(itemRepository.save(itemArgumentCaptor.capture())).thenReturn(item);
 
-        when(itemRepository.save(any(Item.class))).thenReturn(item);
-
+        //Act
         Item result = itemService.createItem(itemDto);
-
+        
+        //Assets
+        Item capturedItem = itemArgumentCaptor.getValue();
+        
         assertNotNull(result);
         assertEquals(1L, result.getId());
         assertEquals("Produto teste", result.getName());
         assertEquals(50.00, result.getValue());
+
+        assertEquals(result.getName(), capturedItem.getName());
+        assertEquals(result.getValue(), capturedItem.getValue());
+    }
+    
+   
+    @Test
+    void shouldThrowException() {
+    	
+    	//Arrange
+        ItemDTO itemDto = new ItemDTO("Produto teste", 50.00);
+        when(itemRepository.save(any(Item.class))).thenThrow(new RuntimeException());
+        
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> itemService.createItem(itemDto));
+    }
+    
+    @Test
+    void shouldThrowExceptionWhenNameIsNull() {
+    	
+    	//Arrange
+    	ItemDTO itemDto = new ItemDTO(null, 50.00);
+    	when(itemRepository.save(any(Item.class))).thenThrow(new IllegalArgumentException());
+    	
+    	// Act & Assert
+    	assertThrows(IllegalArgumentException.class, () -> itemService.createItem(itemDto));
     }
 }
